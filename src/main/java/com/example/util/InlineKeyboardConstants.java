07a7cb616b3a877619d063144b4ae6e7ce4fe0b4
Uktamjon.Sodikov <sodikovuktamjon30@gmail.com>
2025-02-19 18:40:21 +0500

    Big bite bot

--- src/main/java/com/example/util/InlineKeyboardConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.util;
-
-public interface InlineKeyboardConstants {
-    String EXCEL_FILE_DATA = "fileType/excel";
-    String PDF_FILE_DATA = "fileType/pdf";
-    String PDF = "\uD83D\uDCC3PDF";
-    String EXCEL = "\uD83E\uDDFEEXCEL";
-}
